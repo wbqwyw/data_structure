6ecfc77a65cc5441acd893aeea86ddaf513f36a4
wangbq3 <yonyou2016%>
2020-12-24 10:43:17 +0800

    链表增加修改和删除方法以及从前到后的取数，反转逻辑

--- src/main/test/linkedlist/SingleOrderLinkedList.java
+++ src/main/test/linkedlist/SingleOrderLinkedList.java
@@ -1,5 +1,7 @@
 package main.test.linkedlist;
 
+import java.net.HttpRetryException;
+
 /**
  * @ClassName SingleOrderLinkedList
  * @Description 有序链表--和添加元素顺序没有关系，按照heroNode 的 no排序,以及反转链表
@@ -13,20 +15,29 @@ public class SingleOrderLinkedList {
         HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
         HeroNode hero2 = new HeroNode(2, "卢俊义", "玉麒麟");
         HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
-//        HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
+        HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
         //创建要给链表
         SingleOrderLinkedList singleLinkedList = new SingleOrderLinkedList();
         //加入
         singleLinkedList.add(hero1);
         singleLinkedList.add(hero3);
-//        singleLinkedList.add(hero4);
+        singleLinkedList.add(hero4);
         singleLinkedList.add(hero2);
         // 测试一下单链表的反转功能
         System.out.println("原来链表的情况~~");
         singleLinkedList.show();
         System.out.println(singleLinkedList.size());
-        singleLinkedList = singleLinkedList.reverse(singleLinkedList);
+//        singleLinkedList = singleLinkedList.reverse(singleLinkedList);
+        singleLinkedList.reverse2(singleLinkedList);
         System.out.println("=============reverse=============");
+        singleLinkedList.show();
+        System.out.println("=============update=============");
+        HeroNode hero5 = new HeroNode(2, "卢俊义", "玉麒麟~");
+        singleLinkedList.update(hero5);
+        singleLinkedList.show();
+        System.out.println("=============delete=============");
+        HeroNode hero6 = new HeroNode(2, "卢俊义", "玉麒麟~");
+        singleLinkedList.delete(hero6);
         singleLinkedList.show();
     }
 
@@ -93,6 +104,20 @@ public class SingleOrderLinkedList {
         return length;
     }
 
+    /**
+     * @return
+     * @Author wangbq
+     * @Description //链表反转，此方法的原理，新建一个链表（初始化一个head），
+     * 然后依次取出最后一个node,放到新的head后，后续依次插入head.next
+     * 1.计算原链表长度m
+     * 2.指针temp指向head
+     * 3.变量n
+     * 4.让指针往尾部走，走m步长，n++记录步长，当n=m时，及代表走到队尾。取出
+     * 元素，然后m--。temp=sl.head  n=0 初始化归为，从新遍历
+     * 5.当m=0时，表示有效元素全部取出
+     * @Date 9:43 2020/12/24
+     * @Param
+     */
     public SingleOrderLinkedList reverse(SingleOrderLinkedList sl) {
         if (sl.isEmpty()) {
             System.out.println("链表为空");
@@ -120,6 +145,82 @@ public class SingleOrderLinkedList {
         sl = newList;
         return sl;
     }
+
+    /**
+     * @return
+     * @Author wangbq
+     * @Description 链表反转方式2
+     * 原理：先初始化一个head，然后依次从前到后取出元素，连接到head后面，新元素要插入head和next之间。保证反转
+     * 然后，将原head.next = newHead.next
+     * 1.HeroNode newHead = new HeroNode(0,null,null);
+     * 2.HeroNode temp = sl.head.next;
+     * 3. while(true){
+     * if(temp.next==null){
+     * break;
+     * }
+     * temp.next=newHead.next;
+     * newHead.next = temp;
+     * }
+     * sl.head.next = newHead.next;
+     * @Date 9:52 2020/12/24
+     * @Param
+     */
+    public void reverse2(SingleOrderLinkedList sl) {
+        if (isEmpty()) {
+            System.out.println("链表为空");
+        }
+        HeroNode newHead = new HeroNode(0, null, null);
+        HeroNode temp = sl.head.next;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            HeroNode node = new HeroNode(temp.no, temp.name, temp.minName);
+            HeroNode next = newHead.next;
+            newHead.next = node;
+            node.next = next;
+            temp = temp.next;
+        }
+        sl.head.next = newHead.next;
+    }
+
+    public void update(HeroNode node) {
+        if (isEmpty()) {
+            System.out.println("链表为空");
+        }
+        HeroNode temp = head.next;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            if (temp.no == node.no) {
+                temp.name = node.name;
+                temp.minName = node.minName;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public void delete(HeroNode node) {
+        if (isEmpty()) {
+            System.out.println("链表为空");
+        }
+        HeroNode temp = head.next;
+        HeroNode temp_pre = head;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            if (temp.no == node.no) {
+                temp_pre.next = temp.next;
+                break;
+            }
+            temp_pre = temp_pre.next;
+            temp = temp.next;
+        }
+
+    }
+
 }
 
 
